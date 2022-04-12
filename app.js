48c2a5e33c5d834ca0168850c51ddc11e26888f4
keshav <tomarkeshavofficial@gmail.com>
2022-04-12 15:50:07 +0530

    complete

---
+++ app.js
@@ -0,0 +1,91 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const lodash = require("lodash");
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+let app = express();
+
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+const posts = [];
+
+
+app.get("/", (req, res) => {
+
+  // posts.forEach(function(post){               //this method is used to truncate (shorten) the text
+  //   post.content = post.content.substring(0,80) + "...";
+  // })          //but we are going to use it inside the home.ejs
+
+  var options = {
+    startingContent: homeStartingContent,
+    posts: posts
+  }
+
+  res.render("home", options);
+})
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    aboutcontent: aboutContent
+  })
+})
+
+app.get("/contact", (req, res) => {
+  res.render("contact", {
+    contactContent: contactContent
+  })
+})
+
+app.get("/compose", (req, res) => {
+  res.render("compose")
+})
+
+
+app.post("/compose", (req, res) => {
+
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  }
+
+  posts.push(post);
+  res.redirect("/");
+
+})
+
+app.get("/posts/:title", (req, res) => {
+     const requestedTitle = lodash.lowerCase(req.params.title);
+     posts.forEach(function(post){
+       const storedTitle = lodash.lowerCase(post.title);
+
+       //lodash.lowerCase is used to render the website even if user gives lowerCase or uppercase, or dash or underscore, it should match the string
+
+       if(storedTitle === requestedTitle){
+         res.render("post",{
+           title : post.title,
+           content : post.content
+         });
+       }
+     })
+})
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
